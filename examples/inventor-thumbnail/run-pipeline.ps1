e3bac76244b0126bb7f022bf9aae46a05edecf5e
Petr Broz <petr.broz@autodesk.com>
2019-05-30 13:19:40 +0200

    PowerShell example scripts exiit on error.

--- examples/inventor-thumbnail/run-pipeline.ps1
+++ examples/inventor-thumbnail/run-pipeline.ps1
@@ -11,7 +11,11 @@ $input_object_key = "input.ipt"
 $thumbnail_object_key = "thumbnail.bmp"
 
 # If it does not exist, create a data bucket
-$result = Invoke-Expression "$forge_dm_bin list-buckets --short" | Select-String -Pattern $env:FORGE_BUCKET | Measure-Object -Line
+$result = Invoke-Expression "$forge_dm_bin list-buckets --short"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not list buckets" -ErrorAction "Stop"
+}
+$result = $result | Select-String -Pattern $env:FORGE_BUCKET | Measure-Object -Line
 if ($result.Lines -eq 0) {
     Write-Host "Creating a bucket $env:FORGE_BUCKET"
     Invoke-Expression "$forge_dm_bin create-bucket $env:FORGE_BUCKET"
@@ -23,24 +27,39 @@ Invoke-Expression "$forge_dm_bin upload-object $input_file_path application/octe
 New-Item -Name "output" -Path "." -ItemType "directory"
 New-Item -Name "thumbnail.bmp" -Path ".\output" -ItemType "file"
 Invoke-Expression "$forge_dm_bin upload-object .\output\thumbnail.bmp image/bmp $env:FORGE_BUCKET $thumbnail_object_key"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not upload objects" -ErrorAction "Stop"
+}
 
 # Generate signed URLs for all input and output files
 Write-Host "Creating signed URLs"
 $input_file_signed_url = Invoke-Expression "$forge_dm_bin create-signed-url $env:FORGE_BUCKET $input_object_key --access read --short"
 $thumbnail_signed_url = Invoke-Expression "$forge_dm_bin create-signed-url $env:FORGE_BUCKET $thumbnail_object_key --access readwrite --short"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not generate signed URLs" -ErrorAction "Stop"
+}
 
 # Create work item and poll the results
 Write-Host "Creating work item"
 $workitem_id = Invoke-Expression "$forge_da_bin create-workitem $activity_name $activity_alias --input PartFile --input-url $input_file_signed_url --output Thumbnail --output-url $thumbnail_signed_url --short"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not create work item" -ErrorAction "Stop"
+}
 Write-Host "Waiting for work item $workitem_id to complete"
 $workitem_status = "inprogress"
 while ($workitem_status -eq "inprogress") {
     Start-Sleep -s 5
     $workitem_status = Invoke-Expression "$forge_da_bin get-workitem $workitem_id --short"
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Could not obtain work item status" -ErrorAction "Stop"
+    }
     Write-Host $workitem_status
 }
 
 # Download the results
 Write-Host "Downloading results to output/thumbnail.bmp"
 Invoke-Expression "$forge_dm_bin download-object $env:FORGE_BUCKET $thumbnail_object_key output\thumbnail.bmp"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not download results" -ErrorAction "Stop"
+}
 Write-Host "Process complete. See the thumbnail in the output folder, or download it from $thumbnail_signed_url"
--- examples/inventor-thumbnail/setup-pipeline.ps1
+++ examples/inventor-thumbnail/setup-pipeline.ps1
@@ -12,7 +12,11 @@ $activity_alias = "dev"
 
 # Create or update an appbundle
 Write-Host "Creating an appbundle $appbundle_name"
-$result = Invoke-Expression "$forge_da_bin list-appbundles --short" | Select-String -Pattern $appbundle_name | Measure-Object -Line
+$result = Invoke-Expression "$forge_da_bin list-appbundles --short"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not list app bundles" -ErrorAction "Stop"
+}
+$result = $result | Select-String -Pattern $appbundle_name | Measure-Object -Line
 if ($result.Lines -eq 0) {
     Write-Host "Creating new appbundle"
     Invoke-Expression "$forge_da_bin create-appbundle $appbundle_name $appbundle_file $appbundle_engine"
@@ -20,12 +24,23 @@ if ($result.Lines -eq 0) {
     Write-Host "Updating existing appbundle"
     Invoke-Expression "$forge_da_bin update-appbundle $appbundle_name $appbundle_file $appbundle_engine"
 }
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not create or update app bundle" -ErrorAction "Stop"
+}
 
 # Create or update an appbundle alias
 Write-Host "Creating an appbundle alias $appbundle_alias"
-$appbundle_version = Invoke-Expression "$forge_da_bin list-appbundle-versions $appbundle_name --short" | Select-Object -Last 1
+$result = Invoke-Expression "$forge_da_bin list-appbundle-versions $appbundle_name --short"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not list app bundle versions" -ErrorAction "Stop"
+}
+$appbundle_version = $result | Select-Object -Last 1
 Write-Host "Last appbundle version: $appbundle_version"
-$result = Invoke-Expression "$forge_da_bin list-appbundle-aliases $appbundle_name --short" | Select-String -Pattern $appbundle_alias | Measure-Object -Line
+$result = Invoke-Expression "$forge_da_bin list-appbundle-aliases $appbundle_name --short"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not list app bundle aliases" -ErrorAction "Stop"
+}
+$result = $result | Select-String -Pattern $appbundle_alias | Measure-Object -Line
 if ($result.Lines -eq 0) {
     Write-Host "Creating new appbundle alias"
     Invoke-Expression "$forge_da_bin create-appbundle-alias $appbundle_alias $appbundle_name $appbundle_version"
@@ -33,10 +48,17 @@ if ($result.Lines -eq 0) {
     Write-Host "Updating existing appbundle alias"
     Invoke-Expression "$forge_da_bin update-appbundle-alias $appbundle_alias $appbundle_name $appbundle_version"
 }
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not create or update app bundle alias" -ErrorAction "Stop"
+}
 
 # Create or update an activity
 Write-Host "Creating an activity $activity_name"
-$result = Invoke-Expression "$forge_da_bin list-activities --short" | Select-String -Pattern $activity_name | Measure-Object -Line
+$result = Invoke-Expression "$forge_da_bin list-activities --short"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not list activities" -ErrorAction "Stop"
+}
+$result = $result | Select-String -Pattern $activity_name | Measure-Object -Line
 if ($result.Lines -eq 0) {
     Write-Host "Creating new activity"
     Invoke-Expression "$forge_da_bin create-activity $activity_name $appbundle_name $appbundle_alias $appbundle_engine --input PartFile --output Thumbnail --output-local-name thumbnail.bmp"
@@ -44,16 +66,30 @@ if ($result.Lines -eq 0) {
     Write-Host "Updating existing activity"
     Invoke-Expression "$forge_da_bin update-activity $activity_name $appbundle_name $appbundle_alias $appbundle_engine --input PartFile --output Thumbnail --output-local-name thumbnail.bmp"
 }
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not create or update activity" -ErrorAction "Stop"
+}
 
 # Create or update an activity alias
 Write-Host "Creating an activity alias $activity_alias"
-$activity_version = Invoke-Expression "$forge_da_bin list-activity-versions $activity_name --short" | Select-Object -Last 1
+$result = Invoke-Expression "$forge_da_bin list-activity-versions $activity_name --short"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not list activity versions" -ErrorAction "Stop"
+}
+$activity_version = $result | Select-Object -Last 1
 Write-Host "Last activity version: $activity_version"
-$result = Invoke-Expression "$forge_da_bin list-activity-aliases $activity_name --short" | Select-String -Pattern $activity_alias | Measure-Object -Line
+$result = Invoke-Expression "$forge_da_bin list-activity-aliases $activity_name --short"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not list activity aliases" -ErrorAction "Stop"
+}
+$result = $result | Select-String -Pattern $activity_alias | Measure-Object -Line
 if ($result.Lines -eq 0) {
     Write-Host "Creating new activity alias"
     Invoke-Expression "$forge_da_bin create-activity-alias $activity_alias $activity_name $activity_version"
 } else {
     Write-Host "Updating existing activity alias"
     Invoke-Expression "$forge_da_bin update-activity-alias $activity_alias $activity_name $activity_version"
+}
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not create or update activity alias" -ErrorAction "Stop"
 }
